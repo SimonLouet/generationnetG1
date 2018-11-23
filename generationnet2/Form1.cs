dd5d4a1edd646e1b1274631cbc196fd0e1a49ef1
SimonL <simon.louet.98@gmail.com>
2018-11-23 15:47:02 +0100

    connexion BDD

--- generationnet2/Form1.cs
+++ generationnet2/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GenerationNet.dao;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,28 @@ namespace generationnet2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            BDD bdd = BDD.getInstance();
+            string message = "SELECT Client: \n";
+            OracleDataReader test = bdd.getPrepareCommand("Select * FROM Client", new Dictionary<string, object>());
+
+            
+            while (test.Read())
+            {
+                int id = test.GetInt32(test.GetOrdinal("CLI_ID"));
+                string prenom = (String)test["CLI_PRENOM"];
+                string nom = (String)test["CLI_NOM"];
+                message += "id : " + id + "  prenom : " + prenom + "   nom : " + nom + "\n";
+            }
+            label1.Text = message;
         }
     }
 }
--- generationnet2/Program.cs
+++ generationnet2/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using GenerationNet.dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace generationnet2
 {
@@ -17,6 +20,9 @@ namespace generationnet2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            
+            
         }
     }
 }
