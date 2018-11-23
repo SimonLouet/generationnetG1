3348f0a81edd75ac7697cf4c576b6be8e671fe59
SimonL <simon.louet.98@gmail.com>
2018-11-23 15:48:10 +0100

    connexion BDD

---
+++ generationnet2/DAO/BDD.cs
@@ -0,0 +1,53 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GenerationNet.dao
+{
+    //Classe qui représente la bdd
+    class BDD
+    {
+        private static BDD _instance;
+        private OracleConnection _bdd;
+
+        private BDD()
+        {
+            
+            string chainecnx = "DATA SOURCE=172.30.11.12:1521/orcl; PASSWORD = generationnet ;PERSIST SECURITY INFO=True;USER ID=GENERATIONNET";
+            _bdd = new OracleConnection();
+            this._bdd.ConnectionString = chainecnx;
+            //création de la connexion à la BDD oracle             
+            this._bdd.ConnectionString = chainecnx;
+            this._bdd.Open();
+        }
+
+
+        public OracleDataReader getPrepareCommand(String command, Dictionary<string, object> parametre)
+        {
+            OracleCommand cmd = this._bdd.CreateCommand();
+            cmd.CommandText = command;
+            
+            foreach (string x in parametre.Keys)
+            {
+                cmd.Parameters.Add(new OracleParameter(x, parametre[x]));
+            }
+            return cmd.ExecuteReader();
+        }
+
+
+
+        public static BDD getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new BDD();
+            }
+
+            return _instance;
+        }
+    }
+}
